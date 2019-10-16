bbb260f55922a02167c5eff829ce65a3f2669fef
Yasmin Afifi <yasmin.afifi97@gmail.com>
2019-10-16 11:07:50 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HeaderApp from './components/common/header';
+
+
+function App() {
+  return (
+
+    <HeaderApp />
+
+  );
+}
+
+export default App;
---
+++ src/components/common/header.js
@@ -0,0 +1,199 @@
+import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+import Fade from '@material-ui/core/Fade';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import purple from '@material-ui/core/colors/purple';
+import HomeIcon from '@material-ui/icons/Home';
+
+
+
+
+const useStyles = makeStyles(theme => ({
+    grow: {
+        flexGrow: 1,
+
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        alignSelf: "center",
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 200,
+        },
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    button: {
+        color: "#FFFFFF",
+        borderColor: "#FFFFFF",
+
+    },
+    input: {
+        display: 'none',
+    },
+    fab: {
+        marginRight: theme.spacing(2),
+    },
+
+}));
+
+
+
+
+
+function HeaderApp() {
+    const classes = useStyles();
+    const accent = purple['A400'];
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleProfileMenuOpen = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+
+    };
+
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>settings</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Help</MenuItem>
+            <MenuItem onClick={handleMenuClose}>About</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Log out</MenuItem>
+
+        </Menu>
+    );
+    return (
+        <div className={classes.grow}>
+            <AppBar position="static" style={{ backgroundColor: "#f06292" }}>
+                <Toolbar >
+                    <Fab color="default" aria-label="add" className={classes.fab} size="small">
+                        <HomeIcon />
+                    </Fab>
+
+
+                    <Button variant="contained" size="small" color='default' >
+                        Boards
+                    </Button>
+
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
+                    <div className={classes.grow}>
+                        <h2 align="center"> Trello </h2>
+                    </div>
+
+
+
+
+                    <Fab aria-label="add" className={classes.fab} size="small">
+                        <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} title="Add">
+                            <AddIcon />
+                        </Tooltip>
+                    </Fab>
+
+
+                    <div className={classes.sectionDesktop}>
+                        <IconButton aria-label="show 1 new notifications" color="inherit" size="small">
+                            <Badge badgeContent={1} color="default">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>
+
+                        <IconButton
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            color="default"
+                            size="medium"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar >
+            {renderMenu}
+        </ div >
+    );
+}
+export default HeaderApp;
